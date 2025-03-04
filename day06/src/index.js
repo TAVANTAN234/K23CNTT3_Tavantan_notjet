170140bb2a45a8ccd1ce96cdf46358b90a30aa76
tan van <Tavantan113355@gmail.com>
2025-03-04 15:04:00 +0700

    https://github.com/TAVANTAN234/K23CNTT3_Tavantan_notjet.git

--- day06/src/index.js
+++ day06/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TvcApp from './TvcApp';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const tvcRoot = ReactDOM.createRoot(document.getElementById('tvcRoot'));
+tvcRoot.render(
   <React.StrictMode>
-    <App />
+    <TvcApp />
   </React.StrictMode>
 );
 
